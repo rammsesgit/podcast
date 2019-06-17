6e58d291f21a58131a6d902199fed78af4a760cd
Isar Ramses (g) <rammses_@live.com.mx>
2019-06-16 22:21:47 -0500

    redundant channel

--- routes.js
+++ routes.js
@@ -2,5 +2,5 @@ const routes = require('next-routes')
 
 module.exports = routes()
   .add('index')
-  .add('channel', '/channel/:slug.:id', 'channel')
+  .add('channel', '/:slug.:id', 'channel')
   .add('podcast', '/slugChannel.:idChannel/:slug.:id', 'podcast')
